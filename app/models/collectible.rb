5e7c337f9223fe033a916937ee67c5ef14127597
Robert Kim <robert23kim@gmail.com>
2021-03-14 00:28:35 -0500

    implemented collectibles, users, assets inner join

--- app/models/collectible.rb
+++ app/models/collectible.rb
@@ -1,2 +1,4 @@
 class Collectible < ActiveRecord::Base
+    has_many :assets
+    has_many :users, :through => :assets
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+    has_many :assets
+    has_many :collectibles, :through => :assets
 end
