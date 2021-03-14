ccbeb33d77865835d2f684330ad04b7f01f0d94e
Jeremy Lu <jeremylu04@gmail.com>
2021-03-14 19:12:07 +0000

    Can add a specified collectible

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :users
-  
+  resources :users do
+      get 'open_case', :on => :member
+      get 'add_collectible', :on => :member
+  end
+    
   # map '/' to be a redirect to '/users'
   root :to => redirect('/users')
 
