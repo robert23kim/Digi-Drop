5f6b79e3c24fbe442b18f6a3defe300e3743128d
kuobao <alexkuobao@gmail.com>
2021-03-14 23:39:51 -0400

    Delete irrelevant code
    improve test code coverage

--- features/step_definitions/movies_steps.rb
+++ features/step_definitions/movies_steps.rb
@@ -20,18 +20,6 @@ Given /the following assets exist/ do |users_table|
   end
 end
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  expect(page.body.index(e1) < page.body.index(e2))
-end
-
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  rating_list.split(', ').each do |rating|
-    step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
-  end
-end
-
 Then /I should see all the users/ do
   # Make sure that all the movies in the app are visible in the table
   User.all.each do |user|
@@ -47,13 +35,6 @@ Then /I should see all of the collectibles of "(.*)"/ do |username|
   collection.each do |c|
     step %{I should see "#{c.name}"}
   end
-end
-
-#Then(/^I should see (\d+) collectibles for "([^"]*)"$/) do |arg1, arg2|
-#  user = User.where(username: arg2)[0]
-
-Then /^the director of "(.*?)" should be "(.*?)"$/ do |mov, dir|
-  Movie.find_by_title(mov).director.should == dir
 end
 
 Then /(.*) seed users should exist/ do | n_seeds |
