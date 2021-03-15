da69afb88c288272adec5a80cc3ecd97796c2f97
Robert Kim <robert23kim@gmail.com>
2021-03-15 17:00:28 -0400

    implemented application helper rspec

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe ApplicationHelper do
+
+  describe "GET logged_in?" do
+    it "returns true when user is logged in" do
+      user = FactoryGirl.create(:user)
+      session[:user_id] = user.id
+      expect(logged_in?).to be true
+    end
+
+    it "returns false when user is not logged in" do
+      session[:user_id] =  nil
+      expect(logged_in?).to be false
+    end
+  end
+
+  describe "GET current_user" do
+    it "returns current user if the user is logged in" do
+      user = FactoryGirl.create(:user)
+      session[:user_id] = user.id
+      expect(current_user).to eq user
+    end
+
+    it "returns nil if the user is not logged in" do
+      session[:user_id] =  nil
+      expect(assigns(:current_user)).to eq nil
+    end
+  end
+end
