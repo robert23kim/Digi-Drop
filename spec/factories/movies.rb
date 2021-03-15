a43b37624ac3e08725f66083a7da7d89ee2871b9
Robert Kim <robert23kim@gmail.com>
2021-03-14 22:12:21 -0400

    comment out incomplete

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -7,15 +7,4 @@ FactoryGirl.define do
     username "john123"
     password_digest "12345"
   end
-
-  factory :invalid_movie, parent: :movie do
-    title nil
-  end
-
-  factory :no_director_movie, parent: :movie do
-    title "Alien"
-    rating "R"
-    director nil
-    release_date "1979-05-25"
-  end
 end
