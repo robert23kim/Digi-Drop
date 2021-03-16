31e4666916ee40fe7f3c698aa2197152396d9f7e
Robert Kim <robert23kim@gmail.com>
2021-03-15 21:56:17 -0400

    fixed int ids

--- db/migrate/20210314013323_assets.rb
+++ db/migrate/20210314013323_assets.rb
@@ -1,8 +1,8 @@
 class Assets < ActiveRecord::Migration
   def change
     create_table :assets do |t|
-      t.string :user_id
-      t.string :collectible_id
+      t.integer :user_id
+      t.integer :collectible_id
       # Add fields that let Rails automatically keep track
       # of when movies are added or modified:
       t.timestamps
